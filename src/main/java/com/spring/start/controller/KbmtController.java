4394ecae9b415c95d46bc274b1f71d6c0fdf3af4
lalalal <lalalal@LALALAL>
2019-05-16 13:13:07 +0900

    chat

--- src/main/java/com/spring/start/controller/KbmtController.java
+++ src/main/java/com/spring/start/controller/KbmtController.java
@@ -28,6 +28,25 @@ public class KbmtController {
 	
 	@RequestMapping("/search.do")
 	public void search() {
+		
+		//챗봇 질의문 Simul
+		String ichatIp = "http://211.39.140.169"; 
+		String ichatPort = "17300"; 
+		String projectId = "ba2a28894c27"; 
+		String query = "어제 김정은은 어디에 있었어?";
+		String apiURI = ichatIp+":"+ichatPort+"/api/v1/demo/simulation";
+		
+		StringBuilder sb = new StringBuilder();
+		String  jsonRequestBuf  =  "{\r\n" + 
+				"\"projectId\":\""+projectId+"\",\r\n" + 
+				"\"query\":\""+query+"\"\r\n" + 
+				"}"; 
+		String result = service.sendPost(sb.toString(),apiURI, jsonRequestBuf); 
+		System.out.println(jsonRequestBuf);
+		
+		System.out.println(result);
+
+		
 		System.out.println(service.search());
 	}
 }
--- src/main/java/com/spring/start/service/KbmtService.java
+++ src/main/java/com/spring/start/service/KbmtService.java
@@ -1,7 +1,17 @@
 package com.spring.start.service;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,4 +34,59 @@ public class KbmtService {
 	public List<Map<String, String>> search() {
 		return r.srch1("query", "sfield");
 	}
+
+	
+	// HTTP POST request
+	public static String sendPost(String ok, String api, String postBody) {
+		// create json body
+		URL obj;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			obj = new URL(api);
+
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+			con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+			con.setDoOutput(true);
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+
+			wr.write(postBody.toString().getBytes("UTF-8"));
+			// System.out.println(postBody.toString().getBytes("UTF-8"));
+			wr.flush();
+			wr.close();
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+			String inputLine;
+			try {
+				while ((inputLine = in.readLine()) != null) {
+					sb.append(inputLine);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				if (in != null) {
+					try {
+						in.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+				con.disconnect();
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (ConnectException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		String apiResponse = sb.toString();
+		return apiResponse;
+	}
+	
 }
